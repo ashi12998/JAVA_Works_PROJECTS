fe7de97629d7d599ad986a6bd2c6940aa3ebf2a6
Ashish Srivastava <142235212+ashi12998@users.noreply.github.com>
2024-02-13 10:29:19 +0530

    Add files via upload

---
+++ CalculateValueA.java
@@ -0,0 +1,9 @@
+public class CalculateValueA 
+{
+    public static void main(String[] args){
+        int x =7;
+        int a = x *49/7+35/7;
+        System.out.println("The value of a is:" +a);
+    }
+    }
+

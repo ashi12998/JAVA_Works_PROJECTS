9a4b16c1d8fc15b1dc54e252d055379df21657dd
Ashish Srivastava <142235212+ashi12998@users.noreply.github.com>
2024-04-19 11:00:50 +0530

    Add files via upload

---
+++ primeNumberCheck.java
@@ -0,0 +1,25 @@
+public class primeNumberCheck {
+    
+    public static void main(String[] args) {
+        System.out.println(isPrime(49));
+        System.out.println(isPrime(19));
+
+    }
+public static boolean isPrime(int n){
+    if (n==0 || n==1){
+        return false;
+
+    }
+    if (n== 2){
+        return true;
+
+    }
+    for (int i =2 ; i<=n /2; i++){
+        if (n% i == 0){
+            return false;
+
+        }
+    }
+    return true;
+}
+}
